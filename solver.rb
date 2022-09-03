248233b9447cd978844e17ff4b80b98bd068ef58
Debabrata Mandal <bapidebabrata@gmail.com>
2022-09-03 13:28:29 +0530

    Define the fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -6,4 +6,15 @@ class Solver
   def self.reverse(s)
     s.reverse
   end
+  def self.fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
